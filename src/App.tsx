4acafc85e123a27998fe060db502294bdc7b583b
Norbert de Langen <ndelangen@me.com>
2024-06-24 23:07:47 +0200

    chore: Update dependencies and add @typed-tabletop-simulator/lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { ttsUi } from "@typed-tabletop-simulator/ui";
+import { Button, ttsUi } from "@typed-tabletop-simulator/ui";
 
 import { HelloWorld } from "./HelloWorld";
 
 export const App = () => {
   return (
-    <panel color="Yellow" width={200} height={200}>
+    <panel color="Yellow" width={200} height={200} active>
       <HelloWorld />
+      <Button onClick={() => log("clicked")} text="Click me!" />
     </panel>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import { render, ttsUi, ttsUiFragment } from "@typed-tabletop-simulator/ui";
+import { Forge } from "@typed-tabletop-simulator/lib";
 
 import { App } from "App";
 
 onLoad = () => {
   log("Loading done!");
   const ui = render(Global, <App />);
+
+  // const card = Forge.createCard({
+  //   name: "Test Card",
+  //   front: "https://i.imgur.com/1j6Q3QF.png",
+  //   back: "https://i.imgur.com/1j6Q3QF.png",
+  // });
+
+  // log(card);
+
+  // Forge.spawnObject(card, { position: { x: 0, y: 5, z: 0 } });
 
   // for (const obj of getObjects()) {
   //   log(obj.getName());
