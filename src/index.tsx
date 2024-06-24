ae308895c20f0c186e1c6f6557be1581725ea880
Norbert de Langen <ndelangen@me.com>
2024-06-24 23:35:53 +0200

    feat: Create Test Deck with Test Card and spawn object
    This commit creates a Test Deck with a Test Card and spawns the object in the game. The Test Deck is created using the `Forge.createDeck` function with the specified name, front image, back image, width, height, and cards. The `Forge.spawnObject` function is then used to spawn the object in the game at the specified position. This change improves the functionality of the application by adding the ability to create and spawn custom decks.

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,26 @@ onLoad = () => {
   log("Loading done!");
   const ui = render(Global, <App />);
 
-  // const card = Forge.createCard({
-  //   name: "Test Card",
-  //   front: "https://i.imgur.com/1j6Q3QF.png",
-  //   back: "https://i.imgur.com/1j6Q3QF.png",
-  // });
+  const card = Forge.createDeck({
+    name: "Test Deck",
+    front:
+      "http://cloud-3.steamusercontent.com/ugc/2495638295534741822/069CB00E06AF42090B520D9170572DB3EEE84160/",
+    back: "http://cloud-3.steamusercontent.com/ugc/2495638295534742758/A997C7F6F7D52EAFD55ABACDD9C9AEE2F3AD668B/",
+    width: 1,
+    height: 1,
+    cards: [{ index: 1, name: "Test Card" }],
+  });
 
   // log(card);
+  const s = async () => {
+    const out = await Forge.spawnObject(card, {
+      position: { x: 0, y: 5, z: 0 },
+    });
 
-  // Forge.spawnObject(card, { position: { x: 0, y: 5, z: 0 } });
+    log(out);
 
-  // for (const obj of getObjects()) {
-  //   log(obj.getName());
-  // }
+    return;
+  };
+
+  s();
 };
